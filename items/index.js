541d799579ac3d02ede4c1721b0f97be53807448
TaranehKhaleghi <tkhaleghi5062@conestogac.on.ca>
2020-09-27 20:55:12 -0400

    added portfolio to the url

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "title":"MonoGame", "fname":"mono_game.md", "specialImage":"../img/game1.JPG"},
-{"id": "item2", "title":"Order Book", "fname":"order_book.md", "specialImage":"../img/orderbook.JPG"},
-{"id": "item3", "title":"Reservation", "fname":"reservation.md", "specialImage":"../img/reservation3.JPG"},
-{"id": "item4", "title":"MobileApp", "fname":"mobile_app.md", "specialImage":"../img/mobileApp.JPG"}];
+export default [{"id": "item1", "title":"MonoGame", "fname":"mono_game.md", "specialImage":"portfolio/img/game1.JPG"},
+{"id": "item2", "title":"Order Book", "fname":"order_book.md", "specialImage":"portfolio/img/orderbook.JPG"},
+{"id": "item3", "title":"Reservation", "fname":"reservation.md", "specialImage":"portfolio/img/reservation3.JPG"},
+{"id": "item4", "title":"MobileApp", "fname":"mobile_app.md", "specialImage":"portfolio/img/mobileApp.JPG"}];
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 export default [
 {"title":"index", "fname":"index.md"},
-{"title":"About", "fname":"about.md", "specialImage":"../img/about.JPG"},
+{"title":"About", "fname":"about.md", "specialImage":"portfolio/img/about.JPG"},
 {"title":"Contact", "fname":"contact.md"}];
